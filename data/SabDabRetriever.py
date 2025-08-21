7d1d7294cff9a39a8927afa1e30b5509e5e2a456
franklin-hiciano <fhiciano0@gmail.com>
2025-08-21 19:12:19 -0400

    Add/update class SabDabRetriever

--- data/SabDabRetriever.py
+++ data/SabDabRetriever.py
@@ -1,5 +1,16 @@
+import os
+from datetime import datetime
+import contextlib
+import subprocess
+from urllib.request import urlretrieve
+from urllib.request import HTTPError
+from pathlib import Path
+import glob
+import pandas as pd
+from Bio.PDB import PDBParser, PPBuilder
+
 class SabDabRetriever:
-  def __init__(self):    
+  def __init__(self):
     self.summary_file_path = None
     self.structures_dir = None
 
