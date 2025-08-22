71ddc0904c1a4f34d6166bc36b7e7dad761826cb
franklin-hiciano <fhiciano0@gmail.com>
2025-08-22 15:31:12 -0400

    update FranklinLabNB/utils/PDBs.py

--- FranklinLabNB/utils/PDBs.py
+++ FranklinLabNB/utils/PDBs.py
@@ -8,9 +8,18 @@ class PDBs:
     self.pdbs = [Path(f) for f in pdbs]
     self.dir = os.path.dirname(os.path.abspath(self.pdbs[0]))
 
-def __init__(self, pdbs):
-  if not isinstance(pdbs, list): pdbs = [pdbs]
-  self.pdbs = [Path(f) for f in pdbs]
-  self.dir = os.path.dirname(os.path.abspath(self.pdbs[0]))
+def to_fasta(self):
+  pdbprs = PDBParser()
+  ppb = PPBuilder()
+  fastas = []
+  for pdb in self.pdbs:
+    chains = pdbprs.get_structure("p", pdb).get_chains()
+    fasta = os.path.join(pdb.parent, pdb.stem+".fasta")
+    with open(fasta, 'w') as f:
+      for c in chains:
+        amino_acids = "".join(str(pp.get_sequence()) for pp in ppb.build_peptides(c))
+        f.write(f""">{pdb.stem}|{c.id}\n{amino_acids}\n""")
+    fastas.append(fasta)
+  return fastas
 
 PDBs.to_fasta = to_fasta
