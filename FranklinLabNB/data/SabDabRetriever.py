bd0197786c5b17635c8ea53105b6c4aff5bf7286
franklin-hiciano <fhiciano0@gmail.com>
2025-08-22 04:32:12 -0400

    update FranklinLabNB/data/SabDabRetriever.py

--- FranklinLabNB/data/SabDabRetriever.py
+++ FranklinLabNB/data/SabDabRetriever.py
@@ -22,23 +22,28 @@ def get_structures(self):
   subprocess.run([sys.executable, "sabdab_downloader.py", "--summary_file", self.summary_file, "--chothia_pdb", "--output_path", outpath], check=True, stderr=subprocess.STDOUT, stdout=subprocess.PIPE)
   return outpath
 
-def get_sequences_and_structures(self):
-  os.chdir(self.get_structures())
+def get_sequences_and_structures(self, structures_dir=None):
   df = pd.read_csv(self.summary_file, sep="\t")
-  def pdbs():
-    pdb_id = lambda pdb_file: Path(pdb_file).stem
-    chains = lambda pdb_file: PDBParser().get_structure("ab-ag_complex", pdb_file).get_chains()
-    amino_acids = lambda chn: "".join(str(pp.get_sequence) for pp in PPBuilder().build_peptides(chn))
-    return {pdb_id(f): {chn.id: amino_acids(chn) for chn in chains(f)} for f in glob.glob("*/**/*.pdb", recursive=True)}
+  with contextlib.chdir(structures_dir or self.get_structures()):
+    print(os.getcwd())
+    pdbprs = PDBParser()
+    ppb = PPBuilder()
+    def pdbs():
+      pdb_id = lambda pdb_file: Path(pdb_file).stem
+      chains = lambda pdb_file: pdbprs.get_structure("ab-ag_complex", pdb_file).get_chains()
+      amino_acids = lambda chn: ("".join(str(pp.get_sequence()) for pp in ppb.build_peptides(chn)), print(chn.id))[0]
 
-  def insert_seqs_into_table():
-    for pdb_id, chns in pdbs().items():
-      for chn_id, chn_seq in chns.items:
-        pdb_rows = df[df['pdb']==pdb_id]
-        chn_row = pdb_rows[(pdb_rows==chn_id).any(axis=1)]
-        chn_col_name = pdb_rows.columns[(pdb_rows==chn_id).any(axis=0)][0]+'_seq'
-        df.loc[chn_row.index[0], chn_col_name] = chn_seq
-  insert_seqs_into_table()
+      return {pdb_id(f): {chn.id: amino_acids(chn) for chn in chains(f)} for f in glob.glob("*/**/*.pdb", recursive=True)}
+
+    def insert_seqs_into_table():
+      for pdb_id, chns in pdbs().items():
+        for chn_id, chn_seq in chns.items():
+          pdb_rows = df[df['pdb']==pdb_id]
+          chn_row = pdb_rows[(pdb_rows==chn_id[0]).any(axis=1)]
+          if chn_row.empty: continue
+          chn_col_name = pdb_rows.columns[(pdb_rows==chn_id).any(axis=0)][0]+'_seq'
+          df.loc[chn_row.index[0], chn_col_name] = chn_seq
+    insert_seqs_into_table()
 
   df.to_csv((out := Path(self.summary_file).stem+'_with_seqs.tsv'), sep='\t', index=False)
   return out
