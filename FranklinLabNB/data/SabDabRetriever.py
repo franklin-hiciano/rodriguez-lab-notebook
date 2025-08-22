f3fc558504d91b18183fa6ca9d127343a29ab0d0
franklin-hiciano <fhiciano0@gmail.com>
2025-08-22 02:50:36 -0400

    update FranklinLabNB/data/SabDabRetriever.py

--- FranklinLabNB/data/SabDabRetriever.py
+++ FranklinLabNB/data/SabDabRetriever.py
@@ -1,82 +1,34 @@
-import os
-import sys
-from datetime import datetime
-import contextlib
-import subprocess
-from urllib.request import urlretrieve
-from urllib.request import HTTPError
-from pathlib import Path
-import glob
-import pandas as pd
-from Bio.PDB import PDBParser, PPBuilder
-
 class SabDabRetriever:
-  def __init__(self):
-    self.summary_file_path = None
-    self.structures_dir = None
+  def __init__(self, summary_file_download_link):
+    self.timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    self.summary_file = f"sabdab_summary_{self.timestamp}.tsv"
+    urlretrieve("download_link", self.summary_file)
 
-def get_summary_file(self, download_link, destination=None):
-  destination = destination or f"sabdab_summary_{datetime.now().strftime("%Y-%m-%d_%H-%M-%S")}.tsv"
-  try:
-    urlretrieve(download_link, destination)
-    self.summary_file_path = destination
-  except Exception as e:
-    print(e)
-    if "404" in str(e): sys.exit("[ERROR] Try a new SabDab download link, yours may be expired.")
-    else: sys.exit()
-
-def get_structures(self, destination_dir=None):
-  destination_dir = destination_dir or f"sabdab_complexes_{datetime.now().strftime("%Y-%m-%d_%H-%M-%S")}"
+def get_structures(self):
   urlretrieve("https://opig.stats.ox.ac.uk/webapps/sabdab-sabpred/sabdab/downloads/sabdab_downloader.py/", "sabdab_downloader.py")
-  os.makedirs(destination_dir, exist_ok=True)
-  cmd = [
-    sys.executable, "-u", "sabdab_downloader.py",
-    "--summary_file", self.summary_file_path,
-    "--chothia_pdb",
-    "--output_path", destination_dir
-  ]
-  try:
-    env = os.environ.copy()
-    env["PYTHONUNBUFFERED"] = "1"
-    subprocess.run(cmd, env=env, check=True)
-    self.structures_dir = destination_dir
-  except Exception as e:
-    print(f"[ERROR] Error while downloading structures from SabDab: {e}")
-    sys.exit(1)
+  subprocess.run([sys.executable, "sabdab_downloader.py", "--summary_file", self.summary_file, "--chothia_pdb", "--output_path", (dir := f"sabdab_structures_{self.timestamp}")], check=True)
+  return dir
 
-def extract_sequences_from_structures(self, destination=None):
-  destination = destination or Path(self.summary_file_path).stem+".tsv"
-  df_summary = pd.read_csv(self.summary_file_path, sep="\t")
+def get_sequences_and_structures(self):
+  os.chdir(self.get_structures())
+  df = pd.read_csv(self.summary_file, sep="\t")
+  def pdbs():
+    pdb_id = lambda pdb_file: Path(pdb_file).stem
+    chains = lambda pdb_file: PDBParser().get_structure("ab-ag_complex", pdb_file).get_chains()
+    amino_acids = lambda chn: "".join(str(pp.get_sequence) for pp in PPBuilder().build_peptides(chn))
+    return {pdb_id(f): {chn.id: amino_acids(chn) for chn in chains(f)} for f in glob.glob("*/**/*.pdb", recursive=True)}
 
-  with contextlib.chdir(self.structures_dir):
-    pdb_files = glob.glob("*/**/*.pdb", recursive=True)
+  def insert_seqs_into_table():
+    for pdb_id, chns in pdbs().items():
+      for chn_id, chn_seq in chns.items:
+        pdb_rows = df[df['pdb']==pdb_id]
+        chn_row = pdb_rows[(pdb_rows==chn_id).any(axis=1)]
+        chn_col_name = pdb_rows.columns[(pdb_rows==chn_id).any(axis=0)][0]+'_seq'
+        df.loc[chn_row.index[0], chn_col_name] = chn_seq
+  insert_seqs_into_table()
 
-    pdbprs = PDBParser(QUIET=True)
-    ppb = PPBuilder()
+  df.to_csv((out := Path(self.summary_file).stem+'_with_seqs.tsv'), sep='\t', index=False)
+  return out
 
-    def get_pdb_id(pdb_file):
-      return Path(pdb_file).stem
-    def get_chains(pdb_file):
-      return pdbprs.get_structure("ab-ag_complex", pdb_file).get_chains()
-    def get_amino_acids(chain):
-      return "".join(str(pp.get_sequence()) for pp in ppb.build_peptides(chain))
-
-    pdbs = {get_pdb_id(f): {chain.id: get_amino_acids(chain) for chain in get_chains(f)}
-            for f in pdb_files}
-
-    def insert_into_table(pdb_id, chain_id, chain_seq):
-
-      pdb_rows = df_summary[df_summary['pdb'] == pdb_id]
-      chain_row = pdb_rows[(pdb_rows == chain_id).any(axis=1)]
-      chain_col_name = pdb_rows.columns[(pdb_rows == chain_id).any(axis=0)][0] + '_sequence'
-      df_summary.loc[chain_row.index[0], chain_col_name] = chain_seq
-
-    [insert_into_table(pdb_id, chain_id, chain_seq)
-    for pdb_id, chains in pdbs.items()
-    for chain_id, chain_seq in chains.items()]
-
-  df_summary.to_csv(destination, sep='\t', index=False)
-
-SabDabRetriever.get_summary_file = get_summary_file
 SabDabRetriever.get_structures = get_structures
-SabDabRetriever.extract_sequences_from_structures = extract_sequences_from_structures
+SabDabRetriever.get_sequences_and_structures = get_sequences_and_structures
